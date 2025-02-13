1e291d12ff6f8a95c7a90257401323f826e0b907
tharushiwelarathna <nirmanitharushi1@gmail.com>
2025-02-13 10:28:42 +0530

    how to type a web element and say press enter in selenium

--- src/test/java/OpenGoogleTest.java
+++ src/test/java/OpenGoogleTest.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,7 +10,9 @@ public class OpenGoogleTest {
     public void googleTest(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
-        //open wuna browser eka close karana command eka me
-        driver.quit();
+        //open wuna browser eka close karana command eka me - driver.quit();
+        //driver.quit();
+
+        driver.findElement(By.name("q")).sendKeys("Colombo" + Keys.ENTER);
     }
 }
