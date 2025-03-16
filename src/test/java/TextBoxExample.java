f8747e8c84b97ff0969b1dfe83df7654ae4f3557
tharushiwelarathna <nirmanitharushi1@gmail.com>
2025-03-16 10:08:20 +0530

    Implemented text box operations: enter, clear, validate, and retrieve input

---
+++ src/test/java/TextBoxExample.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TextBoxExample {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openLinkTestPage() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize(); //open wechcha browser eka desktop size ekatama fit krla denna kiynne mehemai
+        driver.get("https://www.leafground.com/input.xhtml");
+        Thread.sleep(3000); // thappara ganak wait karanna onanm mehema dala krnna puluwan
+    }
+    @Test
+    public void textBoxTests(){
+        //01) Type your name
+        WebElement name = driver.findElement(By.id("j_idt88:name"));
+        name.sendKeys(" Tharushi Nirmani");
+
+        //02) Append Country to this City.
+        WebElement appendText = driver.findElement(By.id("j_idt88:j_idt91"));
+        appendText.sendKeys("India");
+
+        //03) Verify if text box is disabled
+        boolean enabled = driver.findElement(By.id("j_idt88:j_idt93")).isEnabled();
+        System.out.println(" Is Text Box Enabled :" + enabled );
+
+
+        //04) Clear the typed text.
+        WebElement clearText = driver.findElement(By.xpath("//*[@id='j_idt88:j_idt95']"));
+        clearText.clear();
+
+        //05) Retrieve the typed text.
+        WebElement textElement = driver.findElement(By.id("j_idt88:j_idt97"));
+        String value = textElement.getAttribute("value");
+        System.out.println(value);
+
+        //06) Type email and Tab. Confirm control moved to next element.
+        WebElement email = driver.findElement(By.id("j_idt88:j_idt99"));
+        email.sendKeys("nirmanitharushi1@gmail.com" + Keys.TAB + "Confirm control moved to next element.");
+         }
+
+
+
+}
